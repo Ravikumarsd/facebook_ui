2f58b99bb944a7142933a821be41e6d2a34a9819
Ravikumar Daivajna <ravi@Apples-MacBook-Pro.local>
2018-11-09 00:10:56 +0530

    ignoring node_modules

--- .react-playground/index.js
+++
@@ -1,8 +0,0 @@
-
-        import React from 'react';
-        import ReactDom from 'react-dom';
-        import Playground from 'react-playground-kit';
-        import * as module from '/Users/ravi/internship/facebook_ui/src/components/AuthComponent.js';
-
-        ReactDom.render(React.createElement(Playground, { module }), document.getElementById('app'));
-    
